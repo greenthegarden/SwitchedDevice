b44adaacf3dc8bc4d4827da9e4dd0096a186299a
Philip Cutler <cutlervufamily@gmail.com>
2015-04-23 22:13:44 +0930

    added support for time

--- SwitchedDevice.h
+++ SwitchedDevice.h
@@ -10,7 +10,14 @@
 
 
 #define ON  HIGH
-#define OFF LOW 
+#define OFF LOW
+
+
+#define USE_TIME false
+
+#if USE_TIME
+#include "Time.h"
+#endif
 
 
 class SwitchedDevice
@@ -22,10 +29,18 @@ private:
   boolean _initial_state;
   unsigned long _last_on = 0;
   unsigned long _last_off = 0;
+#if USE_TIME
+  time_t _last_time_on = 0;
+  time_t _last_time_off = 0;
+#endif
 public:
   byte pin();
   unsigned long last_on();
   unsigned long last_off();
+#if USE_TIME
+  time_t last_time_on();
+  time_t last_time_off();
+#endif
   byte state();
   byte switchOn();
   byte switchOff();
